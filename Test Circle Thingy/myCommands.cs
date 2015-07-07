de7d65d3492b9ff641ad555ab806adad33a0c3e9
unknown <loekar01@GISPC07.canamgeomatics.local>
2015-07-07 07:59:21 -0600

    Created solution from template.

---
+++ Test Circle Thingy/myCommands.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Autodesk.AutoCAD.Runtime;
+using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.EditorInput;
+
+// This line is not mandatory, but improves loading performances
+[assembly: CommandClass(typeof(Test_Circle_Thingy.MyCommands))]
+
+namespace Test_Circle_Thingy
+{
+    public class MyCommands
+    {
+        [CommandMethod("MyCommand")]
+        public void MyCommand() // This method can have any name
+        {
+            // Put your command code here
+
+        }
+    }
+}
