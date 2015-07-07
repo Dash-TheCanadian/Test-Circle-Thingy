6461b50361122bdf9c23c34a54d3d6763945a019
unknown <loekar01@GISPC07.canamgeomatics.local>
2015-07-07 08:09:34 -0600

    Added prompts.

--- Test Circle Thingy/myCommands.cs
+++ Test Circle Thingy/myCommands.cs
@@ -4,6 +4,7 @@ using Autodesk.AutoCAD.ApplicationServices;
 using Autodesk.AutoCAD.DatabaseServices;
 using Autodesk.AutoCAD.Geometry;
 using Autodesk.AutoCAD.EditorInput;
+using Toolbox.AutoCAD;
 
 // This line is not mandatory, but improves loading performances
 [assembly: CommandClass(typeof(Test_Circle_Thingy.MyCommands))]
@@ -16,7 +17,31 @@ namespace Test_Circle_Thingy
         public void MyCommand() // This method can have any name
         {
             // Put your command code here
+            PromptPointOptions ppo = new PromptPointOptions("Pick the centre of the circle");
+            PromptPointResult ppr = Active.Editor.GetPoint(ppo);
 
+            if (ppr.Status != PromptStatus.OK)
+                return;
+
+            Point3d pt = ppr.Value;
+
+            PromptDistanceOptions pdo = new PromptDistanceOptions("Pick radius") {
+                UseBasePoint = true,
+                BasePoint = pt,
+                AllowArbitraryInput = true
+            };
+
+            PromptDoubleResult pdr = Active.Editor.GetDistance(pdo);
+
+            if (pdr.Status != PromptStatus.OK)
+                return;
+
+            double dist = pdr.Value;
+
+            using (Transaction tr = Active.Database.TransactionManager.StartTransaction())
+            {
+                
+            }
         }
     }
 }
